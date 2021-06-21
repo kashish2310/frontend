3f502ecbadc888ef5370152a2c5c78756e213717
kashish2310 <85179545+kashish2310@users.noreply.github.com>
2021-06-22 00:04:27 +0530

    Delete 100days.js

--- frontend tha/day 8/100days.js
+++
@@ -1,9 +0,0 @@
-var s = document.querySelectorAll('.grid-item , .color');
-s.forEach(div=>
-  {
-    div.addEventListener("click",function()
-    {
-      div.classList.toggle("clicked")
-    })
-  }
-)  
