8efcdd841ac6c62e695bd3dc8c02ad8d3629e68c
kashish tibrewal <kashish.1923en1149@kiet.edu>
2021-07-08 21:22:11 +0530

    commit

---
+++ frontend tha/day 14/script.js
@@ -0,0 +1,22 @@
+function myfunction(){
+    return document.getElementById('s1').innerHTML= "Hello! how r u?";
+}
+
+function myfunction2(){
+    return document.getElementById('s2').innerText="This is double click";
+}
+
+function myfunction3(){
+    var x= document.getElementById("s3");
+    x.value= x.value.toUpperCase();
+}
+
+function myInputFunction(){
+    var x= document.getElementById("myinput").value;
+    document.getElementById("demo").innerHTML= "You wrtie: "+x;
+}
+
+var y= 0;
+function myScrollfun(){
+    document.getElementById("demo1").innerHTML= y+=1;
+}
